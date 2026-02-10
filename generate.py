56ba20196ea74aec790377ec0d78568fb76bf509
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2026-02-11 02:36:04 +0300

    auto

--- generate.py
+++ generate.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import re
 
+def list_get(L, i, default=''):
+    try:
+        return L[i]
+    except IndexError:
+        return default
+
 from generate_utils import OutFile
 
 for filename in ('cv.html', ): # 'cv-edu.html'):
@@ -8,8 +14,8 @@ for filename in ('cv.html', ): # 'cv-edu.html'):
   with open(f'{base}.html') as f:
     s = f.read();
 
-  R = re.compile('{{(.*?)\|(.*?)}}', re.DOTALL)
+  R = re.compile('{{(.*?)}}', re.DOTALL)
 
-  for i, lang in enumerate(('fr', 'en')):
+  for i, lang in enumerate(('fr', 'en', 'ru')):
     with OutFile(f'{base}.{lang}.html', 'w') as f:
-        f.write(R.sub(lambda m: m.group(i+1), s))
+        f.write(R.sub(lambda m: list_get(m.group(1).split('|'), i, default="NOT TRANSLATED"), s))
